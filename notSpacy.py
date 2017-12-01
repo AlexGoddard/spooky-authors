cb582cb682418920183a2941b8b58016b0392847
Nandadeep Davuluru <aditya.nandadeep@gmail.com>
2017-11-30 16:45:08 -0800

    added spacy api file

---
+++ notSpacy.py
@@ -0,0 +1,105 @@
+import spacy, re
+import pandas as pd
+
+nlp = spacy.load('en')
+train = pd.read_csv("~/Desktop/academics/AI/localProject/train.csv")
+test = pd.read_csv("~/Desktop/academics/AI/localProject/test.csv")
+postagfile = open('./postag.txt', 'w')
+wantedTags = re.compile(r'(\w+)\,\s?u\'NN.?\'|(\w+)\,\s?u\'JJ\'|(\w+)\,\s?u\'VB.\'')
+tempRegex = re.compile(r'\w+')
+
+
+EAPdict = list()
+EAPtags = list()
+MWSsentences = list()
+HPLsentences = list()
+
+def displayPandas(content):
+	return content.head()
+
+
+def getData():
+	for index, row in train.iterrows():
+		if row['author'] == 'EAP':
+			EAPdict.append(row['text'])
+			# print row['text'], row['author']
+		elif row['author'] == 'MWS':
+			MWSsentences.append(row['text'])
+		else:
+			HPLsentences.append(row['text'])
+
+def findSimilarity(sent1, sent2):
+	u1 = unicode(sampleSentence1, "utf-8")
+	u1 = nlp(u1)
+	u2 = unicode(sampleSentence2, "utf-8")
+	u2 = nlp(u2)
+	return u1.similarity(u2)
+
+def print_fine_pos(token):
+    return (token.tag_)
+
+def pos_tags(sentence):
+    sentence = unicode(sentence, "utf-8")
+    tokens = nlp(sentence)
+    tags = []
+    for tok in tokens:
+        tags.append((tok,print_fine_pos(tok)))
+    return tags
+
+def parsePOS(annotatedSentence):
+	words = list()
+	matches = re.findall(wantedTags, str(annotatedSentence))
+	for match in matches:
+		print tempRegex.findall(str(match))
+		words.append(tempRegex.findall(str(match)))
+	return words
+
+def buildWords(sentences, author):
+
+	if author == 'EAP':
+		eapauthorwords = open('./eap.txt', 'w')
+		for sentence in sentences:
+			eapauthorwords.write(str(parsePOS(pos_tags(sentence))))
+			eapauthorwords.write('\n')
+	elif author == 'HPL':
+		hplwordsfile = open('./hpl.txt', 'w')
+		for sentence in sentences:
+			hplwordsfile.write(str(parsePOS(pos_tags(sentence))))
+			hplwordsfile.write('\n')
+	else:
+		mwsfile = open('./mws.txt', 'w')
+		for sentence in sentences:
+			mwsfile.write(str(parsePOS(pos_tags(sentence))))
+			mwsfile.write('\n')
+
+
+def countPOStags(annotatedSentence):
+	matches = wantedTags.findall(str(annotatedSentence))
+	count = 0
+	for match in matches:
+		print tempRegex.findall(str(match))
+		count = count + 1
+	print count
+
+
+getData()
+
+'''
+Testing each module below
+'''
+print "EAP Sentences: {}, MWS Sentences: {}, HPL Sentences: {}".format(len(EAPdict), len(MWSsentences), len(HPLsentences))
+sampleSentence1 = EAPdict[0]
+sampleSentence2 = HPLsentences[3]
+
+print "Sentence 1 pos tag -> {}".format(pos_tags(sampleSentence1))
+print "Sentence 2 pos tag -> {}".format(pos_tags(sampleSentence2))
+print "Score for sentence 2 -> {}".format(parsePOS(pos_tags(sampleSentence1)))
+
+'''
+Calling the counter for counting how many syntactic dependencies in a sentence
+'''
+
+print "".format(countPOStags(pos_tags(sampleSentence2)))
+
+
+
